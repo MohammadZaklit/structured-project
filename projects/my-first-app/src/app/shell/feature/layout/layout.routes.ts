9d359b7755167b64635124df7fa481901ec782ae
MohammadZaklit <MohamadZaklit@gmail.com>
2025-09-13 16:31:23 +0300

    fix route to layout

--- projects/my-first-app/src/app/shell/feature/layout/layout.routes.ts
+++ projects/my-first-app/src/app/shell/feature/layout/layout.routes.ts
@@ -1,17 +1,24 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout';
 
 export const layoutRoutes: Routes = [
-  { path: '', redirectTo: 'account', pathMatch: 'full' },
   {
-    path: 'account',
-    loadComponent: () => import('../../../features/account/account').then((m) => m.Account),
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('../../../features/login/login').then((m) => m.Login),
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('../../../features/register/register').then((m) => m.Register),
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', redirectTo: 'account', pathMatch: 'full' },
+      {
+        path: 'account',
+        loadComponent: () => import('../../../features/account/account').then((m) => m.Account),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('../../../features/login/login').then((m) => m.Login),
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('../../../features/register/register').then((m) => m.Register),
+      },
+    ],
   },
 ];
