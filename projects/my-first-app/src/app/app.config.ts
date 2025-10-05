08af759bc97639ba25c6c768d0c1f496cf09c105
Rami Zaklit <ramizaklit@gmail.com>
2025-10-05 14:32:55 +0300

    theme setup in admin generator

--- projects/my-first-app/src/app/app.config.ts
+++ projects/my-first-app/src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 export const appConfig: ApplicationConfig = {
@@ -13,7 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
