6f819950d9c201a5eabd678439c4b63a8db261c5
Rami Zaklit <ramizaklit@gmail.com>
2025-10-10 00:42:51 +0300

    move table grid features to list view - keep table grid as ui only

--- projects/my-lib/src/lib/elements/ui/table-grid/src/table-grid.interface.ts
+++ projects/my-lib/src/lib/elements/ui/table-grid/src/table-grid.interface.ts
@@ -1,23 +1,11 @@
+import { StandardButton } from '@zak-lib/ui-library/components/standardbutton';
+import { FieldConfig } from '@zak-lib/ui-library/shared';
 import { Observable } from 'rxjs';
 
-export interface TableColumn {
-  field: string;
-  header: string;
-  type: 'string' | 'number' | 'date' | 'long-string';
+export interface TableColumn extends FieldConfig {
   sortable?: boolean;
   filter?: boolean; // For inline filtering
 }
-
-export interface TableAction {
-  label: string;
-  icon?: string;
-  callback: (rowData: any) => void;
-  styleClass?: string;
-  tooltip?: string;
-  disabled?: (rowData: any) => boolean; // Dynamic disable based on row data
-  visible?: (rowData: any) => boolean; // Dynamic visibility based on row data
-}
-
 export interface TableGrid {
   columns: TableColumn[];
   data?: Observable<any[]>;
@@ -32,11 +20,6 @@ export interface TableGrid {
     delete?: boolean;
     view?: boolean;
   };
-  dynamicActions?: TableAction[];
+  dynamicActions?: StandardButton[];
   enableColumnSorting?: boolean; // Enable/disable sorting by column header
-  showQuickSearch?: boolean;
-  showAddButton?: boolean;
-  dynamicHeaderButtons?: TableAction[]; // Buttons to display in front of the add button
-  exportToExcel?: boolean;
-  exportFileName?: string;
 }
--- projects/my-lib/src/lib/elements/ui/table-grid/src/table-grid.ts
+++ projects/my-lib/src/lib/elements/ui/table-grid/src/table-grid.ts
@@ -42,10 +42,8 @@ export class TableGridComponent implements OnChanges {
   @Output() onEdit = new EventEmitter<any>();
   @Output() onView = new EventEmitter<any>();
   @Output() onDelete = new EventEmitter<any>();
-  @Output() onRowReorder = new EventEmitter<any>(); // event.dragIndex and event.dropIndex
-  @Output() onAdvancedSearch = new EventEmitter<void>();
-  @Output() onColumnSort = new EventEmitter<any>(); // event.field, event.order, event.data
-  @Output() onAddItem = new EventEmitter<void>();
+  @Output() onRowReorder = new EventEmitter<any>();
+  @Output() onColumnSort = new EventEmitter<any>();
 
   tableData: WritableSignal<any[]> = signal([]);
   selectedRows: any[] = [];
@@ -90,25 +88,5 @@ export class TableGridComponent implements OnChanges {
     if (this.config()?.enableStaticActions || (this.config()?.dynamicActions || [])?.length > 0)
       colspan++;
     return colspan;
-  }
-
-  exportExcel() {
-    import('xlsx').then((xlsx) => {
-      const worksheet = xlsx.utils.json_to_sheet(this.tableData());
-      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-      this.saveAsExcelFile(excelBuffer, this.config()?.exportFileName || 'table_data');
-    });
-  }
-
-  saveAsExcelFile(buffer: any, fileName: string): void {
-    // import('file-saver').then(FileSaver => {
-    //   let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    //   let EXCEL_EXTENSION = '.xlsx';
-    //   const data: Blob = new Blob([buffer], {
-    //     type: EXCEL_TYPE
-    //   });
-    //   FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-    // });
   }
 }
