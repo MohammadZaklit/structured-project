9ecc64e4a193997c54a7ca2e6c8763ccfdbc201f
Rami Zaklit <ramizaklit@gmail.com>
2025-10-10 00:41:31 +0300

    add features to button element

--- projects/my-lib/src/lib/elements/button/src/button.interface.ts
+++ projects/my-lib/src/lib/elements/button/src/button.interface.ts
@@ -4,4 +4,8 @@ export interface Button {
   label?: string;
   style?: 'default' | 'primary' | 'secondary' | 'back-button' | 'sign-in' | 'create-card';
   onclick(): void;
+  styleClass?: string;
+  tooltip?: string;
+  disabled?: (rowData: any) => boolean; // Dynamic disable based on row data
+  visible?: (rowData: any) => boolean; // Dynamic visibility based on row data
 }
