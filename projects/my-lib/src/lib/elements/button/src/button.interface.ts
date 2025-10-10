e7704e870b57194b8fc980d15dacb0e430b89a5d
Rami Zaklit <ramizaklit@gmail.com>
2025-10-10 23:11:34 +0300

    add button icon

--- projects/my-lib/src/lib/elements/button/src/button.interface.ts
+++ projects/my-lib/src/lib/elements/button/src/button.interface.ts
@@ -3,9 +3,10 @@ export interface Button {
   type?: 'button' | 'submit' | 'clear';
   label?: string;
   style?: 'default' | 'primary' | 'secondary' | 'back-button' | 'sign-in' | 'create-card';
-  onclick(): void;
+  onclick(data?: any): void;
   styleClass?: string;
   tooltip?: string;
+  icon?: string;
   disabled?: (rowData: any) => boolean; // Dynamic disable based on row data
   visible?: (rowData: any) => boolean; // Dynamic visibility based on row data
 }
