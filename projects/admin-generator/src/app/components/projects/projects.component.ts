76ec6f29b38e12cbc8599a524ea7518128b6d46d
Rami Zaklit <ramizaklit@gmail.com>
2025-10-12 19:36:29 +0300

    adjust main component module config

--- projects/admin-generator/src/app/components/projects/projects.component.ts
+++ projects/admin-generator/src/app/components/projects/projects.component.ts
@@ -33,20 +33,7 @@ export class ProjectsComponent implements OnInit {
     ],
   };
 
-  public dbFields: FormFieldConfig[] = [
-    { key: 'name', label: 'Full Name', type: 'InputText', step: 0, required: true },
-    { key: 'dob', label: 'Date of Birth', type: 'DatePicker', step: 0 },
-    {
-      key: 'gender',
-      label: 'Gender',
-      type: 'RadioButton',
-      step: 1,
-      options: [
-        { label: 'Male', value: 'M' },
-        { label: 'Female', value: 'F' },
-      ],
-    },
-  ];
+  public dbFields: FormFieldConfig[] = [];
 
   constructor() {}
 
@@ -83,15 +70,29 @@ export class ProjectsComponent implements OnInit {
       exportToExcel: true,
       exportFileName: '',
     });
+
+    this.dbFields = fields.map((field) => {
+      const newField = {
+        key: field['name'],
+        label: field['label'],
+        type:
+          components.find((component) => component.id === field['componentId'])?.componentName ??
+          'InputText',
+        step: 1,
+      };
+      return newField;
+    });
   }
 
   private mapFieldsToColumns(fields: GenericRecord[]): TableColumn[] {
     return (
       fields.map((field) => {
         return {
-          name: field['field_name'],
-          label: field['field_label'],
-          type: field['type'],
+          name: field['name'],
+          label: field['label'],
+          type:
+            components.find((component) => component.id === field['componentId'])?.componentName ??
+            'InputText',
           sortable: true,
           filter: true,
         };
@@ -103,3 +104,165 @@ export class ProjectsComponent implements OnInit {
     console.warn('data; ', data);
   }
 }
+
+export const components = [
+  {
+    id: 1,
+    componentName: 'AutoComplete',
+    label: 'AutoComplete',
+    dbType: 'int',
+  },
+  {
+    id: 2,
+    componentName: 'CascadeSelect',
+    label: 'CascadeSelect',
+    dbType: 'int',
+  },
+  {
+    id: 3,
+    componentName: 'ColorPicker',
+    label: 'ColorPicker',
+    dbType: 'text',
+  },
+  {
+    id: 4,
+    componentName: 'DatePicker',
+    label: 'DatePicker',
+    dbType: 'date',
+  },
+  {
+    id: 5,
+    componentName: 'FloatLabel',
+    label: 'FloatLabel',
+    dbType: 'text',
+  },
+  { id: 6, componentName: 'IconField', label: 'IconField', dbType: 'text' },
+  {
+    id: 7,
+    componentName: 'InputGroup',
+    label: 'InputGroup',
+    dbType: 'text',
+  },
+  { id: 8, componentName: 'InputMask', label: 'InputMask', dbType: 'text' },
+  {
+    id: 9,
+    componentName: 'InputNumber',
+    label: 'InputNumber',
+    dbType: 'numeric',
+  },
+  {
+    id: 10,
+    componentName: 'InputText',
+    label: 'InputText',
+    dbType: 'text',
+  },
+  {
+    id: 11,
+    componentName: 'KeyFilter',
+    label: 'KeyFilter',
+    dbType: 'text',
+  },
+  { id: 12, componentName: 'Knob', label: 'Knob', dbType: 'numeric' },
+  {
+    id: 13,
+    componentName: 'Listbox',
+    label: 'Listbox',
+    dbType: 'many_to_many',
+  },
+  {
+    id: 14,
+    componentName: 'MultiSelect',
+    label: 'MultiSelect',
+    dbType: 'many_to_many',
+  },
+  {
+    id: 15,
+    componentName: 'RadioButtonDB',
+    label: 'Radio Button From DB',
+    dbType: 'foreign_key',
+  },
+  {
+    id: 16,
+    componentName: 'RadioButtonOptions',
+    label: 'Radio Button Static Options',
+    dbType: 'text',
+  },
+  {
+    id: 17,
+    componentName: 'SelectButton',
+    label: 'SelectButton',
+    dbType: 'text',
+  },
+  { id: 18, componentName: 'Slider', label: 'Slider', dbType: 'numeric' },
+  {
+    id: 19,
+    componentName: 'Textarea',
+    label: 'Textarea',
+    dbType: 'longtext',
+  },
+  {
+    id: 20,
+    componentName: 'ToggleButton',
+    label: 'ToggleButton',
+    dbType: 'bool',
+  },
+  {
+    id: 21,
+    componentName: 'ToggleSwitch',
+    label: 'ToggleSwitch',
+    dbType: 'bool',
+  },
+  {
+    id: 22,
+    componentName: 'TreeSelectDB',
+    label: 'TreeSelect From DB',
+    dbType: 'foreign_key',
+  },
+  {
+    id: 23,
+    componentName: 'TreeSelectOptions',
+    label: 'TreeSelect Static Options',
+    dbType: 'text',
+  },
+  { id: 24, componentName: 'Button', label: 'Button', dbType: 'ui' },
+  {
+    id: 25,
+    componentName: 'SplitButton',
+    label: 'SplitButton',
+    dbType: 'ui',
+  },
+  {
+    id: 26,
+    componentName: 'PickListDB',
+    label: 'PickList From DB',
+    dbType: 'many_to_many',
+  },
+  {
+    id: 27,
+    componentName: 'PickListOptions',
+    label: 'PickList Static Data',
+    dbType: 'json',
+  },
+  {
+    id: 28,
+    componentName: 'FileUploadDragDrop',
+    label: 'FileUpload (Drag & Drop)',
+    dbType: 'many_to_many',
+  },
+  {
+    id: 29,
+    componentName: 'FileUploadBrowse',
+    label: 'FileUpload (Browse)',
+    dbType: 'many_to_many',
+  },
+  { id: 30, componentName: 'row', label: 'Row', dbType: 'ui' },
+  { id: 31, componentName: 'column', label: 'Column', dbType: 'ui' },
+  {
+    id: 32,
+    componentName: 'DateTimePicker',
+    label: 'DateTimePicker',
+    dbType: 'datetime',
+  },
+  { id: 33, componentName: 'Rating', label: 'Rating', dbType: 'numeric' },
+  { id: 34, componentName: 'SpeedDial', label: 'SpeedDial', dbType: 'ui' },
+];
