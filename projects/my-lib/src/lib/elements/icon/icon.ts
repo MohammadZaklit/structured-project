a5741948ebd28ddc3dc0b196f6c4fd8ec2f004eb
Rami Zaklit <ramizaklit@gmail.com>
2025-12-27 12:43:43 +0200

    icon technical component setup

---
+++ projects/my-lib/src/lib/elements/icon/icon.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+export interface NzIcon {}
+@Component({
+  selector: 'nz-icon',
+  imports: [],
+  template: ``,
+  styles: ``,
+})
+export class NzIconComponent {}
